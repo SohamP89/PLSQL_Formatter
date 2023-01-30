44cec86da6c0fcc341fd2cf1b9793046fad3972f
sohampaik <soham.paik@gmail.com>
2023-01-30 14:08:14 -0700

    Completely redesigned the GUI in order to make it flow from top to bottom. Still can't get the components to align properly nor can I get the scroll bar working

--- src/Formatter.java
+++ src/Formatter.java
@@ -1,22 +1,34 @@
 /*
 Purpose: Organize PL/SQL statements into a specific format to improve readability and organization. This makes it
 easier for both developers and support engineers to manage databases and fix any bugs/issues.
- */
 
-/*
 From now on, for any GUI applications in Java, use JGoodies instead of default Swing
-because it provides modern, clean, and easy-to-use layouts.
+because it provides modern, clean, and easy-to-use layouts. Alternatively, you could just use GridBagLayout.
  */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
+// Use Box.createRigidArea to make sure that the textboxes aren't oversized
+// when you use them
 public class Formatter extends JFrame implements ActionListener {
     JPanel panel = new JPanel();
-    JPanel subPanel1 = new JPanel();
-    JTextArea textField1 = new JTextArea();
-    JTextArea textField2 = new JTextArea();
+
+    JPanel subPanel1 = new JPanel(new FlowLayout());
+    JPanel subPanel2 = new JPanel(new FlowLayout());
+    JPanel subPanel3 = new JPanel(new FlowLayout());
+    JLabel label1 = new JLabel("Original:");
+    JLabel label2 = new JLabel("Formatted:");
+
+    JTextArea textArea1 = new JTextArea();
+    JTextArea textArea2 = new JTextArea();
+    JScrollPane scrollPane1 = new JScrollPane(textArea1);
+    JScrollPane scrollPane2 = new JScrollPane(textArea2);
     JButton button1 = new JButton("Format");
+    JMenuBar menuBar1 = new JMenuBar();
+    JMenu file;
+    JMenuItem newOption;
+    JMenuItem quitOption;
     public static void main(String[] args) {
         Formatter gui = new Formatter();
     }
@@ -24,19 +36,34 @@ public class Formatter extends JFrame implements ActionListener {
     public Formatter() {
         // Defines title, size, default operation, and layout of window
         super("PL/SQL Formatter");
-        setSize(600, 600);
+        setSize(800, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        panel.setLayout(new BorderLayout());
 
-        textField1.setPreferredSize(new Dimension(500, 200));
-        button1.setPreferredSize(new Dimension(100, 100));
-        textField2.setPreferredSize(new Dimension(500, 200));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        subPanel1.add(textField1);
-        subPanel1.add(button1);
-        subPanel1.add(textField2);
+        textArea1.setPreferredSize(new Dimension(525, 100));
+        textArea1.setLineWrap(true);
+        textArea1.setWrapStyleWord(true);
+        scrollPane1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        textArea2.setPreferredSize(new Dimension(525, 100));
+        textArea2.setLineWrap(true);
+        textArea2.setWrapStyleWord(true);
+
+        subPanel1.add(label1);
+        subPanel1.add(textArea1);
+        subPanel1.setAlignmentY(Component.TOP_ALIGNMENT);
+
+        subPanel2.add(button1);
+        subPanel2.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+        subPanel3.add(label2);
+        subPanel3.add(textArea2);
+        subPanel3.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 
         panel.add(subPanel1);
+        panel.add(subPanel2);
+        panel.add(subPanel3);
         add(panel);
         setVisible(true);
     }
