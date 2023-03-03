428ad4635ca6ea6b393df546973abe67e0857ac2
sohampaik <soham.paik@gmail.com>
2023-03-03 10:46:18 -0700

    Deleted some comments

--- src/SqlFormatter.java
+++ src/SqlFormatter.java
@@ -6,7 +6,11 @@ From now on, for any GUI applications in Java, use JGoodies instead of default S
 because it provides modern, clean, and easy-to-use layouts. Alternatively, you could just use GridBagLayout.
  */
 
+/*
+Source for code that copies text to clipboard: https://stackoverflow.com/questions/6710350/copying-text-to-the-clipboard-using-java
+ */
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.datatransfer.StringSelection;
@@ -18,14 +22,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-
-/*
-Used this website as a reference: https://stackoverflow.com/questions/6710350/copying-text-to-the-clipboard-using-java
- */
-
 public class SqlFormatter extends JFrame implements ActionListener {
-    JPanel panel = new JPanel(new GridBagLayout());
-    JPanel textPanel1 = new JPanel(new FlowLayout());
+
+    /* Definition and Declaration of Java Swing components */
     JPanel subPanel1 = new JPanel(new FlowLayout());
     JPanel subPanel2 = new JPanel(new FlowLayout());
     JPanel subPanel3 = new JPanel(new FlowLayout());
@@ -34,30 +33,33 @@ public class SqlFormatter extends JFrame implements ActionListener {
     JLabel label2 = new JLabel("Formatted SQL:");
 
     JTextArea textArea1 = new JTextArea();
-    JScrollPane scrollPane1 = new JScrollPane(subPanel1, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     JTextArea textArea2 = new JTextArea();
-    JScrollPane scrollPane2 = new JScrollPane(textArea2);
     JButton formatButton = new JButton("Format");
     JButton clearButton = new JButton("Clear");
     JButton downloadButton = new JButton("Download");
     JButton copyButton = new JButton("Copy to Clipboard");
     JFileChooser fc = new JFileChooser();
-    //JScrollBar vbar = new JScrollBar(JScrollBar.VERTICAL, 100, 100, 100, 500);
 
+    /* ---- Main Method ----
+     * Creates new instance of constructor in order to run the main program */
     public static void main(String[] args) {
         SqlFormatter gui = new SqlFormatter();
     }
 
+    /* ---- Constructor ----
+     * Sets the title, size, layout, and close operation of the program.
+     * Adds the Swing components to the plane */
     public SqlFormatter() {
-        // Defines title, size, default operation, and layout of window
+        /* Defines title, size, default operation, and layout of window */
         super("SQL Formatter");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        /* Initialization of key Swing components here */
+        JPanel panel = new JPanel(new GridBagLayout());
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        GridBagConstraints c = new GridBagConstraints();
-
+        /* Sets the size and wrap styles of the Textareas */
         textArea1.setPreferredSize(new Dimension(1250, 350));
         textArea1.setLineWrap(true);
         textArea1.setWrapStyleWord(true);
@@ -66,55 +68,33 @@ public class SqlFormatter extends JFrame implements ActionListener {
         textArea2.setLineWrap(true);
         textArea2.setWrapStyleWord(true);
 
-        scrollPane1.setSize(100, 500);
-//        scrollPane2.setSize(100, 100);
-
+        /* Divides the Swing components into sub-panels */
         subPanel1.add(label1);
         subPanel1.add(textArea1);
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.gridx = 1;
-//        c.gridy = 1;
-//        panel.add(subPanel1, c);
 
         formatButton.addActionListener(SqlFormatter.this);
         subPanel2.add(formatButton);
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.gridx = 0;
-//        c.gridy = 2;
 
         clearButton.addActionListener(SqlFormatter.this);
         subPanel2.add(clearButton);
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.gridx = 1;
-//        c.gridy = 2;
-//        panel.add(subPanel2, c);
 
         subPanel3.add(label2);
         subPanel3.add(textArea2);
-//        //subPanel3.add(scrollPane2);
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.gridx = 1;
-//        c.gridy = 3;
-//        panel.add(subPanel3, c);
 
         downloadButton.addActionListener(SqlFormatter.this);
         subPanel4.add(downloadButton);
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.gridx = 0;
-//        c.gridy = 4;
 
         copyButton.addActionListener(SqlFormatter.this);
         subPanel4.add(copyButton);
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.gridx = 1;
-//        c.gridy = 4;
-//        panel.add(subPanel4, c);
 
+
+        /*  Adds sub-panels to main panel  */
         panel.add(subPanel1);
         panel.add(subPanel2);
         panel.add(subPanel3);
         panel.add(subPanel4);
 
+        /*  Adds panel to main frame  */
         add(panel);
         setVisible(true);
     }
@@ -138,13 +118,28 @@ public class SqlFormatter extends JFrame implements ActionListener {
         }
 
         if ( e.getSource() == downloadButton ) {
+
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("SQL File (.sql)", "sql");
+            fc.setFileFilter(filter);
+
             int returnVal = fc.showSaveDialog(SqlFormatter.this);
+
             if ( returnVal == JFileChooser.APPROVE_OPTION ) {
+
+                String filename;
+
+                if ( !fc.getSelectedFile().getName().contains(".") ) {
+                    filename = fc.getSelectedFile() + ".sql";
+                    fc.setSelectedFile(new File(filename));
+                }
+
                 FileSaveAction();
             }
         }
     }
 
+    /* This method saves the "Formatted Output" text to a file with the
+     *  specified name and the selected directory */
     private void FileSaveAction() {
         try {
             File file = fc.getSelectedFile();
@@ -158,19 +153,23 @@ public class SqlFormatter extends JFrame implements ActionListener {
         }
     }
 
+    /* This method formats the original SQL statement inputted by the user */
     private void formatSql() {
-        // Put next token into a specific variable instead of just using nextToken()
-        // Because nextToken() skips and prints every other token, which messes up your output
 
+        /* Tokenizer splits each keyword, function, and special character of the original SQL
+        * into substrings */
         StringTokenizer st = new StringTokenizer(textArea1.getText());
         String token;
-        String lastToken;
-        String output;
-        int asThere = 0;
-        int isLeftThere = 0;
 
-        String[] KEYWORDS = {"SELECT", "FROM", "WHERE", "AND", "WITH",
-        "CASE", "COUNT(", "SUM("};
+        /* The int variable asThere checks how many times the user has used the AS keyword
+         * If the user has called the AS keyword followed by an open parentheses, then the
+         * program uses the variable to adjust the formatting. */
+        int asThere = 0;
+
+        /* The int variable isLeftThere checks how many times the user has called the LEFT keyword
+         * By checking how often LEFT has been called, the program adjusts the formatting, so that LEFT
+         * is properly aligned. */
+        int isLeftThere = 0;
 
         String[] FUNCTIONS = {"DATE_FORMAT", "SUM", "CHAR_LENGTH", "AVG",
         "COUNT", "FIRST", "LAST", "MAX", "MIN", "UCASE", "LCASE", "MID",
@@ -178,94 +177,73 @@ public class SqlFormatter extends JFrame implements ActionListener {
 
         while ( st.hasMoreTokens() ) {
             token = st.nextToken();
-            System.out.println(token);
+
             if ( token.equalsIgnoreCase("SELECT") ) {
-
-//                if ( asThere == 2 ) {
-//                    textArea2.append("\n            " + token.toUpperCase() + " ");
-//                }
-//                else {
-//                    textArea2.append("\n" + token.toUpperCase() + " ");
-//                }
-
                 textArea2.append("\n" + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("FROM") ) {
-//                System.out.println("\nFROM() found!\n");
-//                if ( asThere == 2) {
-//                    textArea2.append("\n" +
-//                            "              " + token.toUpperCase() + " ");
-//                }
-//                else {
-//
-//                    textArea2.append("\n  " + token.toUpperCase() + " ");
-//                }
-
                 textArea2.append("\n  " + token.toUpperCase() + " ");
-
             }
+
             else if ( token.contains("FROM(") ) {
                 textArea2.append("\n               " + token.toUpperCase() );
             }
+
             else if ( token.equalsIgnoreCase("WHERE") ) {
-
-//                if ( asThere == 2) {
-//                    textArea2.append("\n" +
-//                            "            " + token.toUpperCase() + " ");
-//                }
-//                else
-//                    textArea2.append("\n " + token.toUpperCase() + " ");
-
                 textArea2.append("\n " + token.toUpperCase() + " ");
-
             }
+
             else if ( token.equalsIgnoreCase("AND") ) {
-                //textArea2.append("\n       " + token.toUpperCase() + " ");
                 textArea2.append("\n     " + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("WITH") ) {
                 textArea2.append("\n   " + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("CASE") ) {
-                textArea2.append("\n       " + token.toUpperCase() + " ");
+                textArea2.append("\n    " + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("COUNT(") || token.toUpperCase().startsWith("COUNT(") ) {
-//                textArea2.append("            " + token.toUpperCase());
-                System.out.println("\nFound it!\n");
+//                System.out.println("\nFound it!\n");
                 textArea2.append(token.toUpperCase());
             }
+
             else if ( token.equalsIgnoreCase("LEFT") ) {
                 isLeftThere += 1;
                 textArea2.append("\n    " + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("ORDER") ) {
                 textArea2.append("\n " + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("BY") ) {
                 textArea2.append(token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("NOT") ) {
                 textArea2.append(token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("IN") ) {
                 textArea2.append(token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("NULL") ) {
                 textArea2.append(token.toUpperCase() + " ");
             }
-            else if ( token.equalsIgnoreCase("GROUP") ) {
-//                if ( asThere == 2 ) {
-//                    textArea2.append("\n" +
-//                            "            " + token.toUpperCase() + " ");
-//                }
-//                else
-//                    textArea2.append("\n " + token.toUpperCase() + " ");
 
+            else if ( token.equalsIgnoreCase("GROUP") ) {
                 textArea2.append("\n " + token.toUpperCase() + " ");
             }
+
             else if ( token.equals("AND") ) {
                 textArea2.append("\n    " + token + " ");
             }
+
             else if ( token.equalsIgnoreCase("AS") ) {
                 asThere += 1;
                 textArea2.append(" " + token.toUpperCase() + " ");
@@ -278,14 +256,13 @@ public class SqlFormatter extends JFrame implements ActionListener {
                     textArea2.append(" " + token.toUpperCase() + " ");
                     isLeftThere = 0;
                 }
-
             }
 
             else if ( token.equalsIgnoreCase("UNION") ) {
                 textArea2.append("\n" + token.toUpperCase() + " ");
             }
 
-            else if ( token.equals("(") || token.contains("(") ) {
+            else if ( token.contains("(") ) {
                 asThere += 1;
 //                if ( asThere == 2) {
 //                    textArea2.append("\n" + token + " ");
@@ -303,7 +280,7 @@ public class SqlFormatter extends JFrame implements ActionListener {
                     textArea2.append(token + " ");
                 }
                 else if ( token.endsWith(")") ) {
-                    textArea2.append(token + " ");////////askjldalksjdlkasjdlka
+                    textArea2.append(token + " ");
 
                     boolean functionFound = false;
 
@@ -311,6 +288,7 @@ public class SqlFormatter extends JFrame implements ActionListener {
                          FUNCTIONS) {
                         if ( token.contains(func) ) {
                             functionFound = true;
+                            break;
                         }
                     }
 
@@ -322,22 +300,26 @@ public class SqlFormatter extends JFrame implements ActionListener {
                 }
                 else {
                     String replcStr = token.replace(",", ",\n            ");
-                    System.out.println("String: {" + replcStr + "}");
+//                    System.out.println("String: {" + replcStr + "}");
                     textArea2.append(replcStr);
                 }
             }
+
             else if ( token.startsWith(",") ) {
                 String replcStr = token.replace(",", ",\n           ");
-                System.out.println("String: " + replcStr);
+//                System.out.println("String: " + replcStr);
                 textArea2.append(replcStr);
             }
+
             else if ( token.equals("*/") ) {
                 textArea2.append(" " + token + "\n            ");
             }
+
             else if ( token.contains("*/") ) {
                  String replcStr = token.replace("*/", " */\n            ");
                  textArea2.append(replcStr);
             }
+
             else if ( token.contains(")") ) {
                 if ( asThere == 2) {
                     asThere = 0;
@@ -347,19 +329,24 @@ public class SqlFormatter extends JFrame implements ActionListener {
                     textArea2.append(token + " ");
                 }
             }
+
             else if ( token.equalsIgnoreCase("WHEN") ) {
                 textArea2.append("\n" +
-                        "        " + token.toUpperCase() + " ");
+                        "  " + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("THEN") ) {
-                textArea2.append("\n        " + token.toUpperCase() + " ");
+                textArea2.append("\n   " + token.toUpperCase() + " ");
             }
+
             else if ( token.equalsIgnoreCase("ELSE") ) {
                 textArea2.append("\n        " + token.toUpperCase() + " ");
             }
+
             else {
                 textArea2.append(token + " ");
             }
+
         }
     }
 
